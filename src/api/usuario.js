ccde654d7253324820b4208e9ae6397ff5f8b27f
Jonathan Aviles <avilesgomezjonathan@hotmail.com>
2026-06-13 18:54:15 -0600

    ignore

--- src/api/usuario.js
+++ src/api/usuario.js
@@ -101,4 +101,8 @@ controller.eliminarUsuario = (req, res) => {
         })         
     }
 }
+function validarEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
 module.exports = controller;
