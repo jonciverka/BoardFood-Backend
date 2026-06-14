c24eb432d6285dd9a960698e19b4d57290d27b05
Jonathan Aviles <avilesgomezjonathan@hotmail.com>
2026-06-13 23:10:19 -0600

    tablero

--- src/api/tablero.js
+++ src/api/tablero.js
@@ -42,7 +42,7 @@ controller.obtenerTableros = (req, res) => {
             FROM T_USUARIOS_TIEMPO
             INNER JOIN C_TIEMPOS ON TUS_FK_TIEMPO = CTI_PK_TIEMPO
             LEFT JOIN T_TIEMPO_COMIDA ON CTI_PK_TIEMPO = TTC_FK_TIMEPO 
-            LEFT JOIN T_COMIDAS ON TTC_FK_COMIDA = TCO_PK_COMIDA
+            INNER JOIN T_COMIDA ON TTC_FK_COMIDA = TCO_PK_COMIDA
             WHERE TUS_FK_USUARIO = ?
             AND TCO_ESTADO = 1;
             `,[ pkUsuario],
